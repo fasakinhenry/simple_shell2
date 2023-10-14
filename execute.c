0a55cec571b060c222c5d28eeb7ef3efb75c7408
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-14 10:58:37 +0100

    Feature: Add the command execution function
    makes running of commands without path possible

--- execute.c
+++ execute.c
@@ -3,8 +3,9 @@
 /**
  * execute_command - execute commands if command is detected
  * @args: command to be executed
-*/
-int execute_command(char **args)
+ */
+
+int execute_commands(char **args)
 {
 	char *path = _getenv("PATH");
 	char *delim = ":";
@@ -14,7 +15,7 @@ int execute_command(char **args)
 	{
 		char *path_with_slash = _strconcat(dir, "/");
 		char *full_path = _strconcat(path_with_slash, args[0]);
-		
+
 		printf("Checking path: %s\n", full_path);
 
 		if (path_with_slash != NULL && full_path != NULL)
@@ -58,4 +59,97 @@ int execute_command(char **args)
 	}
 
 	return -1;
+}
+
+int execute_command(char **args)
+{
+	if (args[0] == NULL)
+		return (0);
+	if (_strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] != NULL)
+		{
+			if (chdir(args[1]) != 0)
+			{
+				perror("cd");
+				return (-1);
+			}
+		}
+		return (0);
+	}
+	if (access(args[0], X_OK) == 0)
+	{
+		pid_t child = fork();
+
+		if (child == -1)
+		{
+			perror("problem");
+			return (-1);
+		} else if (child == 0)
+		{
+			execve(args[0], args, environ);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		} else
+		{
+			int status, exit_status;
+
+			waitpid(child, &status, 0);
+			if ((status & 255) == 0)
+				exit_status = (status >> 8) & 255;
+			else
+				exit_status = -1;
+			return (exit_status);
+		}
+	} else
+	{
+		char path[256], *binDirectory = "/bin/";
+		int i = 0, j = 0;
+
+		while (binDirectory[i] != '\0')
+		{
+			path[i] = binDirectory[i];
+			i++;
+		}
+
+		while (args[0][j] != '\0')
+		{
+			path[i] = args[0][j];
+			i++;
+			j++;
+		}
+
+		path[i] = '\0';
+		if (access(path, X_OK) == 0)
+		{
+			pid_t child = fork();
+
+			if (child == -1)
+			{
+				perror("problem");
+				return (-1);
+			} else if (child == 0)
+			{
+				execve(path, args, environ);
+				perror("execve");
+				exit(EXIT_FAILURE);
+			} else
+			{
+				int status;
+				int exit_status;
+
+				waitpid(child, &status, 0);
+				if ((status & 255) == 0)
+					exit_status = (status >> 8) & 255;
+				else
+					exit_status = -1;
+				return (exit_status);
+
+			}
+		} else
+		{
+			write(STDOUT_FILENO, "Command not found.\n", 19);
+			return (-1);
+		}
+	}
 }
