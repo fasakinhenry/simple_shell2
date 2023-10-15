a25c0d3ad7653b3a0b280fee9851f68258b3692c
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-15 10:29:09 +0100

    feat: Add the strings2.c file
    contain function that converts string to int for the exit status command

---
+++ strings2.c
@@ -0,0 +1,28 @@
+/**
+ * string_to_int_conv - Converts string to integer
+ * @str - String to be converted
+ *
+ * return: result * the sign
+ * this tells us if the integer converted to is +ve or -ve
+*/
+int string_to_int_conv(const char *str) {
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    if (str[0] == '-') {
+        sign = -1;
+        i = 1;
+    }
+
+    for (; str[i] != '\0'; i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            result = result * 10 + (str[i] - '0');
+        } else {
+            /* Handle invalid characters here if needed */
+            return 0; /* Return 0 for invalid input */
+        }
+    }
+
+    return (result * sign);
+}
