1a2dd95fb5c49968e0338a0af9a8efa4b22383fb
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-16 14:53:08 +0100

    Style: Align the code to the ALX betty style

--- strings2.c
+++ strings2.c
@@ -1,28 +1,34 @@
 /**
  * string_to_int_conv - Converts string to integer
- * @str - String to be converted
+ * @str: String to be converted
  *
- * return: result * the sign
+ * Return: result * the sign
  * this tells us if the integer converted to is +ve or -ve
 */
-int string_to_int_conv(const char *str) {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
 
-    if (str[0] == '-') {
-        sign = -1;
-        i = 1;
-    }
+int string_to_int_conv(const char *str)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    for (; str[i] != '\0'; i++) {
-        if (str[i] >= '0' && str[i] <= '9') {
-            result = result * 10 + (str[i] - '0');
-        } else {
-            /* Handle invalid characters here if needed */
-            return 0; /* Return 0 for invalid input */
-        }
-    }
+	if (str[0] == '-')
+	{
+		sign = -1;
+		i = 1;
+	}
 
-    return (result * sign);
+	for (; str[i] != '\0'; i++)
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			result = (result * 10 + (str[i] - '0'));
+		} else
+		{
+			/* Handle invalid characters here if needed */
+			return (0); /* Return 0 for invalid input */
+		}
+	}
+
+	return (result * sign);
 }
