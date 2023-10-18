62bef072e43aa9eae3ea097772fd3c3f77e69d96
Fasakin Henry <fasakinhenry@gmail.com>
2023-10-18 09:45:30 +0100

    Fix: Aligned shell.c code to the betty style'

--- shell.c
+++ shell.c
@@ -1,103 +1,106 @@
 #include "main.h"
 
 /**
- * main - This is the main code for the shell
+ * main - Entry point for the shell
  *
- * Return: on success, 0
- * on error, -1 is returned and the errno is set appropriately
-*/
-
+ * Return: On success, returns 0. On error, returns -1 and sets errno.
+ */
 int main(void)
 {
 	while (1)
 	{
 		char **args = get_input();
-		int i;
 
 		if (args == NULL)
 		{
-			write(STDOUT_FILENO, "Error occurred while getting input.\n", 36);
+			perror("Error occurred while getting input.");
 			continue;
 		}
 
 		if (_strchr(args[0], '/') != NULL)
 		{
-			if  (args[0] != NULL && access(args[0], X_OK) == 0)
-			{
-				pid_t child = fork();
-				int i;
-
-				if (child == -1)
-				{
-					perror("problem");
-					exit(EXIT_FAILURE);
-				}
-				else if (child == 0)
-				{
-					execve(args[0], args, environ);
-					perror("execve");
-					for (i = 0; args[i] != NULL; i++)
-						free(args[i]);
-					free(args);
-					exit(EXIT_FAILURE);
-				}
-				else
-				{
-					wait(NULL);
-					for (i = 0; args[i] != NULL; i++)
-						free(args[i]);
-					free(args);
-				}
-			}
+			if (args[0] != NULL && access(args[0], X_OK) == 0)
+				execute_external_command(args);
 			else
-			{
-				int i;
-				for (i = 0; args[i] != NULL; i++)
-				{
-					free(args[i]);
-				}
-				free(args);
-			}
+				cleanup_arguments(args);
 		}
 		else
 		{
-			if (_strcmp(args[0], "exit") == 0) {
-				int i;
-				if (args[1] != NULL) {
-					/* Normal/defualt exit status 
-					int exitstatus;*/
-					int exit_status = string_to_int_conv(args[1]);
-					exit(exit_status);
-				}
-				for (i = 0; args[i] != NULL; i++) {
-					free(args[i]);
-				}
-				free(args);
-				exit(0);
-            		} else if (_strcmp(args[0], "setenv") == 0)
-            		{
-                		setenv_builtin(args);
-            		}
-            		else if (_strcmp(args[0], "unsetenv") == 0)
-            		{
-                		unsetenv_builtin(args);
-            		}
-			
-			/*
-			if (execute_command(args) == -1)
+			if (_strcmp(args[0], "exit") == 0)
 			{
-				write(STDOUT_FILENO, "Command not found.\n", 19);
-				for (i = 0; args[i] != NULL; i++)
-					free(args[i]);
-				free(args);
-			}*/
-			execute_command(args);
-
-			for (i = 0; args[i] != NULL; i++) {
-				free(args[i]);
+				handle_exit(args);
 			}
-			free(args);
-			
+			else if (_strcmp(args[0], "setenv") == 0)
+			{
+				setenv_builtin(args);
+			}
+			else if (_strcmp(args[0], "unsetenv") == 0)
+			{
+				unsetenv_builtin(args);
+			}
+			else
+			{
+				execute_command(args);
+			}
+			cleanup_arguments(args);
 		}
 	}
+}
+
+/**
+ * execute_external_command - Execute an external command.
+ * @args: The arguments for the command.
+ */
+void execute_external_command(char **args)
+{
+	pid_t child = fork();
+
+	if (child == -1)
+	{
+		perror("Fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (child == 0)
+	{
+		execve(args[0], args, environ);
+		perror("execve");
+		cleanup_arguments(args);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+		cleanup_arguments(args);
+	}
+}
+
+/**
+ * handle_exit - Handle the "exit" command.
+ * @args: The arguments for the command.
+ */
+void handle_exit(char **args)
+{
+	if (args[1] != NULL)
+	{
+		int exit_status = string_to_int_conv(args[1]);
+
+		exit(exit_status);
+	}
+	cleanup_arguments(args);
+	exit(0);
+}
+
+/**
+ * cleanup_arguments - Free memory used by argument array.
+ * @args: The argument array to clean up.
+ */
+void cleanup_arguments(char **args)
+{
+	int i;
+
+	for (i = 0; args[i] != NULL; i++)
+	{
+		free(args[i]);
+	}
+	free(args);
 }
